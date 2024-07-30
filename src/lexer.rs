17d3d739a30db46751c3fbd4268c866972f70925
Krzysztof <krzysztof.jan.andrzejewski@gmail.com>
2024-07-30 17:25:23 +0200

    simplified lexing

--- src/lexer.rs
+++ src/lexer.rs
@@ -4,94 +4,46 @@ pub fn lex(input: &str) -> Vec<Ins> {
     let filtered: Vec<char> = input.chars().filter(|c| "<>+-.,[]".contains(*c)).collect();
 
     let mut output: Vec<Ins> = Vec::new();
+    let mut loop_stack: Vec<usize> = Vec::new();
 
-    let mut loop_stack: Vec<i64> = Vec::new();
-
-    let mut ip: i64 = 0;
     for c in filtered {
         match c {
-            '<' => {
-                if !output.last().is_none() && output.last().unwrap().op == Op::Mov {
-                    output[(ip - 1) as usize].arg -= 1;
-                } else {
-                    output.push(Ins {
-                        op: Op::Mov,
-                        arg: -1,
-                    });
-                    ip += 1;
+            '<' | '>' => {
+                let arg = if c == '<' { -1 } else { 1 };
+                if let Some(last) = output.last_mut() {
+                    if let Op::Mov = last.op {
+                        last.arg += arg;
+                        continue;
+                    }
                 }
+                output.push(Ins { op: Op::Mov, arg });
             }
-            '>' => {
-                if !output.last().is_none() && output.last().unwrap().op == Op::Mov {
-                    output[(ip - 1) as usize].arg += 1;
-                } else {
-                    output.push(Ins {
-                        op: Op::Mov,
-                        arg: 1,
-                    });
-                    ip += 1;
+            '+' | '-' => {
+                let arg = if c == '+' { 1 } else { -1 };
+                if let Some(last) = output.last_mut() {
+                    if let Op::Add = last.op {
+                        last.arg += arg;
+                        continue;
+                    }
                 }
+                output.push(Ins { op: Op::Add, arg });
             }
-            '+' => {
-                if !output.last().is_none() && output.last().unwrap().op == Op::Add {
-                    output[(ip - 1) as usize].arg += 1;
-                } else {
-                    output.push(Ins {
-                        op: Op::Add,
-                        arg: 1,
-                    });
-                    ip += 1;
-                }
-            }
-            '-' => {
-                if !output.last().is_none() && output.last().unwrap().op == Op::Add {
-                    output[(ip - 1) as usize].arg -= 1;
-                } else {
-                    output.push(Ins {
-                        op: Op::Add,
-                        arg: -1,
-                    });
-                    ip += 1;
-                }
-            }
-            '.' => {
-                output.push(Ins {
-                    op: Op::Prt,
-                    arg: 0,
-                });
-                ip += 1;
-            }
-            ',' => {
-                output.push(Ins {
-                    op: Op::Inp,
-                    arg: 0,
-                });
-                ip += 1;
-            }
+            '.' => output.push(Ins { op: Op::Prt, arg: 0 }),
+            ',' => output.push(Ins { op: Op::Inp, arg: 0 }),
             '[' => {
-                output.push(Ins {
-                    op: Op::Lst,
-                    arg: 0,
-                });
-                loop_stack.push(ip);
-                ip += 1;
+                output.push(Ins { op: Op::Lst, arg: 0 });
+                loop_stack.push(output.len() - 1);
             }
             ']' => {
                 let start = loop_stack.pop().expect("Unmatched ]");
-                output.push(Ins {
-                    op: Op::Led,
-                    arg: start,
-                });
-                output[start as usize].arg = ip;
-                ip += 1;
+                output.push(Ins { op: Op::Led, arg: start as i64 });
+                output[start].arg = output.len() as i64 - 1;
             }
-            _ => {
-                panic!("invalid character {}", c)
-            }
+            _ => panic!("Invalid character {}", c),
         }
     }
 
-    if loop_stack.len() > 0 {
+    if !loop_stack.is_empty() {
         panic!("Unmatched [");
     }
 
